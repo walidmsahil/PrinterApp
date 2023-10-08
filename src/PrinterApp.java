db6fede1b78bdf94495a12ccb16fad32d4d94d97
walidmsahil <walidjahinsahil@gmail.com>
2023-10-08 20:47:18 +0300

    Positioning the declarations
    Moving all the variable declarations at the top

--- src/PrinterApp.java
+++ src/PrinterApp.java
@@ -3,6 +3,14 @@ public class PrinterApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//Declaring 2 numeric variables and assigning values to them
+		int number1 = 10;
+		int number2 = 5;
+//Declaring variables to find sum, product, difference and quotient
+		int multiplication;
+		int addition;
+		int subtraction;
+		double division;
 //Print "Printer App" to the console
 	System.out.println("Printer App");
 //Print “This app is made by:” to the console
@@ -11,28 +19,26 @@ public class PrinterApp {
 	String creator = "Walid Jahin Mazumder";
 //Print my name next to last line
 	System.out.println(creator);
-//Declaring 2 numeric variables and assigning values to them
-	int number1 = 10;
-	int number2 = 5;
+
 //Print number1 to console
 	System.out.println(number1);
 	System.out.println("The value of number1 is " + number1);
 //Print number2 to console
 	System.out.println("The value of number2 is " + number2);
 //Finding the product of the two numbers
-	int multiplication;
+	
 	multiplication = number1 * number2;
 	System.out.println(number1 + "*" + number2 + "=" + multiplication);
 //Finding the sum
-	int addition;
+	
 	addition = number1 + number2;
 	System.out.println(number1 + "+" + number2 + "=" + addition);
 //Finding the difference
-	int subtraction;
+	
 	subtraction = number1 - number2;
 	System.out.println(number1 + "-" + number2 + "=" + subtraction);
 //Finding the quotient
-	double division;
+	
 	division = number1/number2;
 	System.out.println(number1 + "/" + number2 + "=" + division);
 	
