bc997aa46937d5eb679b2644472595dd9ef97214
walidmsahil <walidjahinsahil@gmail.com>
2023-10-08 19:36:23 +0300

    App name
    Print “Printer App” to the console

--- src/PrinterApp.java
+++ src/PrinterApp.java
@@ -3,6 +3,9 @@ public class PrinterApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//Print "Printer App" to the console
+	System.out.println("Printer App");
+	
 
 	}
 
