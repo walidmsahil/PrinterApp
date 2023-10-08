b5fd08fc203e9e4224166688ee54e845ff8150d0
walidmsahil <walidjahinsahil@gmail.com>
2023-10-08 19:38:33 +0300

    Creator declaration sentence
    Print “This app is made by:” to the console

--- src/PrinterApp.java
+++ src/PrinterApp.java
@@ -5,6 +5,8 @@ public class PrinterApp {
 		// TODO Auto-generated method stub
 //Print "Printer App" to the console
 	System.out.println("Printer App");
+//Print “This app is made by:” to the console
+	System.out.println("This app is made by:");
 	
 
 	}
